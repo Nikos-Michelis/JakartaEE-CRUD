08c56830140944d4f6cac99e5addb4fe47af6612
Νίκος Μιχελής <92666389+Nikos-Michelis@users.noreply.github.com>
2025-01-25 13:35:36 +0200

    Update custom.js

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -3,14 +3,14 @@ function initializePopup() {
   const showPopupBtnAdd = document.querySelector(".add-button");
   const closeButton = formPopup.querySelector(".close-btn");
 
-  // Show login popup on button click
+  // Show popup
   if (showPopupBtnAdd) {
     showPopupBtnAdd.addEventListener("click", () => {
       document.body.classList.toggle("show-popup");
     });
   }
 
-  // Optional: Add an event listener to close the popup when clicking on the close button
+  // close the popup
   if (closeButton) {
     closeButton.addEventListener('click', function () {
       document.body.classList.remove("show-popup");
@@ -22,9 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const autoPopupSection = document.querySelector(".auto-popup");
 
   if (autoPopupSection) {
-    // Automatically show the form-popup for pages with the specific class
     document.body.classList.add('show-popup');
-    // Optional: Add an event listener to handle file input change
     const fileInput = autoPopupSection.querySelector('#fileImage');
     const imageLabel = autoPopupSection.querySelector('.image-upload-label');
     const previewImage = imageLabel.querySelector('img');
@@ -33,29 +31,21 @@ document.addEventListener("DOMContentLoaded", function () {
     function handleFileSelect(event) {
       console.log("File input changed");
       const selectedFile = event.target.files[0];
-
       if (selectedFile) {
         const reader = new FileReader();
-
         reader.onload = function (e) {
           previewImage.src = e.target.result;
         };
-
         reader.readAsDataURL(selectedFile);
       }
     }
   } else {
-    // Initialize popup interactions only if the add-button is present on the page
     initializePopup();
   }
-
-  // Check if there are errors and show the popup if errors exist
   const errorsExist = document.querySelector(".error-message");
   if (errorsExist) {
     document.body.classList.add("show-popup");
   }
-
-  // Optional: Add an event listener to close the popup
   const closeButton = document.querySelector(".close-btn");
   if (closeButton) {
     closeButton.addEventListener('click', function () {
